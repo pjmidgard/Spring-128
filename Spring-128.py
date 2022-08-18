5d453f16d8deb43d3bd5fed9898ab178f3946192
Java Software <pj.midgard@yahoo.com>
2022-08-18 16:40:23 +0100

    Update Spring-128.py

--- Spring-128.py
+++ Spring-128.py
@@ -64,7 +64,7 @@ class compression:
                     name_cut2=len(".paq8o.Spring-128")
                     name_cut1=len(".bin.Spring-128")
                     
-                    if nameas[name_bofore-(name_cut*2):]==".Spring-128.Spring-128" or nameas[name_bofore-(name_cut1):]==".bin.Spring-128" or nameas[name_bofore-(name_cut2):]!=".paq.Spring-128":
+                    if nameas[name_bofore-(name_cut*2):]==".Spring-128.Spring-128" or nameas[name_bofore-(name_cut1):]==".bin.Spring-128" or nameas[name_bofore-(name_cut2):]==".paq.Spring-128":
                         
                     	print("Right format.")
                     
